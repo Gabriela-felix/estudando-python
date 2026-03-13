edc1cc771d94888697210229026b911252ea2c31
Gabriela-felix <gabriellafelix_@hotmail.com>
2026-03-13 01:16:27 +0000

    ainda testando variáveis.

---
+++ variaveis01.py
@@ -0,0 +1,4 @@
+nome = input ('Qual seu nome?')
+idade = input ('Qual sua idade?')
+profissao = input ('Qual sua profissao?')
+print ('Seu nome é',nome,'sua idade é',idade,'e sua profissão é',profissao)
